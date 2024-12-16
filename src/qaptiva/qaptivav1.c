dbe9df3e966784d79ef97fbed724918d7e317a04
di93coc <kayaercument95@gmail.com>
2024-12-16 15:04:20 +0000

    ✨ pre-test work

--- src/qaptiva/qaptivav1.c
+++ src/qaptiva/qaptivav1.c
@@ -1,5 +1,6 @@
 
 
+#include "qdmi/common/enums.h"
 #include <Python.h>
 #include <qdmi/device.h>
 #include <stdio.h>
@@ -27,6 +28,70 @@ typedef struct QDMI_Job_impl_d {
   size_t results_size;
 
 } QDMI_Job_impl_t;
+
+typedef struct QDMI_Site_impl_d {
+  size_t id;
+} QDMI_Site_impl_t;
+
+const QDMI_Site DEVICE_SITES[] = {
+    &(QDMI_Site_impl_t){0},  &(QDMI_Site_impl_t){1},
+    &(QDMI_Site_impl_t){2},  &(QDMI_Site_impl_t){3},
+    &(QDMI_Site_impl_t){4},  &(QDMI_Site_impl_t){5},
+    &(QDMI_Site_impl_t){6},  &(QDMI_Site_impl_t){7},
+    &(QDMI_Site_impl_t){8},  &(QDMI_Site_impl_t){9},
+    &(QDMI_Site_impl_t){10}, &(QDMI_Site_impl_t){11},
+    &(QDMI_Site_impl_t){12}, &(QDMI_Site_impl_t){13},
+    &(QDMI_Site_impl_t){14}, &(QDMI_Site_impl_t){15},
+    &(QDMI_Site_impl_t){16}, &(QDMI_Site_impl_t){17},
+    &(QDMI_Site_impl_t){18}, &(QDMI_Site_impl_t){19},
+    &(QDMI_Site_impl_t){20}, &(QDMI_Site_impl_t){21},
+    &(QDMI_Site_impl_t){22}, &(QDMI_Site_impl_t){23},
+    &(QDMI_Site_impl_t){24}, &(QDMI_Site_impl_t){25},
+    &(QDMI_Site_impl_t){26}, &(QDMI_Site_impl_t){27},
+    &(QDMI_Site_impl_t){28}, &(QDMI_Site_impl_t){29},
+    &(QDMI_Site_impl_t){30}, &(QDMI_Site_impl_t){31},
+    &(QDMI_Site_impl_t){32}, &(QDMI_Site_impl_t){33},
+    &(QDMI_Site_impl_t){34}, &(QDMI_Site_impl_t){35},
+    &(QDMI_Site_impl_t){36}, &(QDMI_Site_impl_t){37},
+    &(QDMI_Site_impl_t){38}
+
+};
+
+#define ADD_STRING_PROPERTY(prop_name, prop_value, prop, size, value,          \
+                            size_ret)                                          \
+  {                                                                            \
+    if ((prop) == (prop_name)) {                                               \
+      if ((value) != NULL) {                                                   \
+        if ((size) < strlen(prop_value) + 1) {                                 \
+          return QDMI_ERROR_INVALIDARGUMENT;                                   \
+        }                                                                      \
+        strncpy((char *)(value), prop_value, (size) - 1);                      \
+        ((char *)(value))[(size) - 1] = '\0';                                  \
+      }                                                                        \
+      if ((size_ret) != NULL) {                                                \
+        *(size_ret) = strlen(prop_value) + 1;                                  \
+      }                                                                        \
+      return QDMI_SUCCESS;                                                     \
+    }                                                                          \
+  }                                                                            \
+
+
+#define ADD_SINGLE_VALUE_PROPERTY(prop_name, prop_type, prop_value, prop,      \
+                                  size, value, size_ret)                       \
+  {                                                                            \
+    if ((prop) == (prop_name)) {                                               \
+      if ((value) != NULL) {                                                   \
+        if ((size) < sizeof(prop_type)) {                                      \
+          return QDMI_ERROR_INVALIDARGUMENT;                                   \
+        }                                                                      \
+        *(prop_type *)(value) = prop_value;                                    \
+      }                                                                        \
+      if ((size_ret) != NULL) {                                                \
+        *(size_ret) = sizeof(prop_type);                                       \
+      }                                                                        \
+      return QDMI_SUCCESS;                                                     \
+    }                                                                          \
+  } 
 
 static QDMI_Device_Status *QDMI_get_device_status(void) {
   static QDMI_Device_Status device_status = QDMI_DEVICE_STATUS_OFFLINE;
@@ -187,10 +252,6 @@ void *submit_job(void *arg) {
 
   pJob->results_size = resultSize;
 
-  // Py_XDECREF(pKeys);
-  // Py_XDECREF(pProbability);
-  // Py_XDECREF(pResults);
-
   Py_Finalize();
 
   pJob->status = QDMI_JOB_STATUS_DONE;
@@ -226,16 +287,19 @@ int QDMI_control_get_data_dev(QDMI_Job job, QDMI_Job_Result result, size_t size,
   if (result == QDMI_JOB_RESULT_PROBABILITIES_SPARSE_VALUES) {
     data = job->probability_values;
     *size_ret *= sizeof(double);
+    return QDMI_SUCCESS;
   }
 
   if (result == QDMI_JOB_RESULT_PROBABILITIES_SPARSE_KEYS) {
     data = job->probability_keys;
+    return QDMI_SUCCESS;
   }
 
   if (result == QDMI_JOB_RESULT_PROBABILITIES_DENSE) {
-    char *bitstream;
+    double *data_ptr = data;
     if (job->probability_dense == NULL) {
-      
+      char *bitstream;
+
       asprintf(&bitstream, "%s", job->probability_keys);
       char *key = strtok(bitstream, ",");
 
@@ -249,36 +313,98 @@ int QDMI_control_get_data_dev(QDMI_Job job, QDMI_Job_Result result, size_t size,
       int stream_index = 0;
       memset(job->probability_dense, 0, sizeof(double) * n_state);
 
-      double *data_ptr = data;
-      
       while (key != NULL) {
         int index = strtol(key, &endptr, 2);
         key = strtok(NULL, ",");
+        job->probability_dense[index] = job->probability_values[stream_index];
         data_ptr[index] = job->probability_values[stream_index++];
       }
-      
       *size_ret = sizeof(double) * n_state;
+    } else {
+      for (int index = 0; index < 1 << job->n_state; index++) {
+        data_ptr[index] = job->probability_dense[index];
+      }
     }
+
+    return QDMI_SUCCESS;
   }
 
+  return QDMI_ERROR_NOTSUPPORTED;
+}
+
+void QDMI_control_free_job_dev(QDMI_Job job) {
+  free(job->probability_dense);
+  free(job->probability_keys);
+  free(job->probability_values);
+
+  free(job->qlm_job);
+  free(job->results);
+
+  free(job);
+}
+
+int C_QDMI_control_finalize_dev(void) {
+  QDMI_set_device_status(QDMI_DEVICE_STATUS_OFFLINE);
   return QDMI_SUCCESS;
 }
 
+int QDMI_query_get_sites_dev(const size_t num_entries, QDMI_Site *sites,
+                             size_t *num_sites) {
+  if ((sites != NULL && num_entries == 0) ||
+      (sites == NULL && num_sites == NULL)) {
+    return QDMI_ERROR_INVALIDARGUMENT;
+  }
+  const size_t device_sites_size =
+      sizeof(DEVICE_SITES) / sizeof(DEVICE_SITES[0]);
+  if (sites != NULL) {
+    const size_t copy_size =
+        (num_entries < device_sites_size ? num_entries : device_sites_size);
+    memcpy((void *)sites, (const void *)DEVICE_SITES,
+           copy_size * sizeof(QDMI_Site));
+  }
+  if (num_sites != NULL) {
+    *num_sites = device_sites_size;
+  }
+  return QDMI_SUCCESS;
+}
 
-/*------------------------------------------------------------------------------
-Copyright 2024 Munich Quantum Software Stack Project
+int QDMI_query_device_property_dev(QDMI_Device_Property prop, size_t size,
+                                   void *value, size_t *size_ret) {
+  if (prop >= QDMI_DEVICE_PROPERTY_MAX || (value == NULL && size_ret == NULL)) {
+    return QDMI_ERROR_INVALIDARGUMENT;
+  }
+  ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_NAME, "QLM", prop,
+                      size, value, size_ret)
+  ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_VERSION, "0.0.1", prop, size, value,
+                      size_ret)
+  ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_LIBRARYVERSION, "0.0.1", prop, size,
+                      value, size_ret)
+  ADD_SINGLE_VALUE_PROPERTY(QDMI_DEVICE_PROPERTY_QUBITSNUM, size_t, 38, prop,
+                            size, value, size_ret)
+  ADD_SINGLE_VALUE_PROPERTY(QDMI_DEVICE_PROPERTY_STATUS, QDMI_Device_Status,
+                            QDMI_read_device_status(), prop, size, value,
+                            size_ret)
+  //Since it is a emulator, there is no coupling map.                        
+  return QDMI_ERROR_NOTSUPPORTED;
+}
 
-Licensed under the Apache License, Version 2.0 with LLVM Exceptions (the
-"License"); you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+int QDMI_query_get_operations_dev(size_t num_entries,
+                                  QDMI_Operation *operations,
+                                  size_t *num_operations) {
+  //Since it is a emulator, there is no native gate set.     
+  return QDMI_ERROR_NOTSUPPORTED;
+}
 
-https://github.com/Munich-Quantum-Software-Stack/QDMI/blob/develop/LICENSE
+int QDMI_query_site_property_dev(QDMI_Site site, QDMI_Site_Property prop,
+                                 size_t size, void *value, size_t *size_ret) {
+  //Since it is a emulator, there is no qubit property.     
+  return QDMI_ERROR_NOTSUPPORTED;
+}
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-License for the specific language governing permissions and limitations under
-the License.
-
-SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-------------------------------------------------------------------------------*/
+int QDMI_query_operation_property_dev(QDMI_Operation operation,
+                                      size_t num_sites, const QDMI_Site *sites,
+                                      QDMI_Operation_Property prop, size_t size,
+                                      void *value, size_t *size_ret) {
+  //Since it is a emulator, there is no operation property.     
+  return QDMI_ERROR_NOTSUPPORTED;
+}

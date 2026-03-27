357ae55cafacb97879ab0c3f0c71e48b60e0a1b4
Martin Knudsen <martin.knudsen@wmi.badw.de>
2026-03-27 13:37:38 +0100

    remove cJSON from post_launch_command

--- .devcontainer/post_create_commands.sh
+++ .devcontainer/post_create_commands.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-cd .. 
+cd ..
 # doxygen
-wget https://www.doxygen.nl/files/doxygen-1.14.0.linux.bin.tar.gz 
+wget https://www.doxygen.nl/files/doxygen-1.14.0.linux.bin.tar.gz
 tar -xzf doxygen-1.14.0.linux.bin.tar.gz
-cd doxygen-1.14.0 
-make 
-make install 
-cd .. 
-ln -s /usr/local/bin/doxygen /usr/bin/doxygen 
-rm -rf doxzyen* 
+cd doxygen-1.14.0
+make
+make install
+cd ..
+ln -s /usr/local/bin/doxygen /usr/bin/doxygen
+rm -rf doxzyen*
 # gtest
 git clone https://github.com/google/googletest.git
 cd googletest
 mkdir build
 cd build
 cmake ..
-make 
+make
 make install
 cd ../..
 rm -rf googletest

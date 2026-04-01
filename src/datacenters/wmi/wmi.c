6ebd96460b18308968b692394f93ab71051cbd08
mortaza-lrz <mortaza.aghtar@lrz.de>
2026-04-01 09:14:06 +0200

    removed qir codes

--- src/datacenters/wmi/wmi.c
+++ src/datacenters/wmi/wmi.c
@@ -168,17 +168,6 @@ const WMI_QDMI_Operation DEVICE_OPERATIONS[] = {
     (struct WMI_QDMI_Operation_impl_d *)&(WMI_QDMI_Operation_impl_t){"mz", 2,
                                                                      0}};
 
-// needed ?
-const char *gate_set[] = {
-    "__quantum__qis__cnot__body", "__quantum__qis__cz__body",
-    "__quantum__qis__id__body",   "__quantum__qis__h__body",
-    "__quantum__qis__x__body",    "__quantum__qis__y__body",
-    "__quantum__qis__z__body",    "__quantum__qis__t__body",
-    "__quantum__qis__s__body",    "__quantum__qis__sx__body",
-    "__quantum__qis__rx__body",   "__quantum__qis__ry__body",
-    "__quantum__qis__rz__body",   "__quantum__qis__mz__body",
-};
-
 // import api token
 char *get_token() {
 
